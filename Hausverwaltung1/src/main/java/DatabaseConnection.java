73fac1b5ae0fb788098c3cf973213e0bbfad4e25
AlbertoARZ <berti02berti02@gmail.com>
2024-10-11 10:42:21 +0200

    Change Interface DataBaseConnection

--- Hausverwaltung1/src/main/java/DatabaseConnection.java
+++ Hausverwaltung1/src/main/java/DatabaseConnection.java
@@ -1,11 +1,12 @@
 public interface DatabaseConnection {
+
     public void IDatabaseConnection(Properties properties);
 
     void createrAllTables();
 
      void truncateAllTables();
 
-      void removeAllTables();
+     void removeAllTables();
 
       void closeConnection();
 }
