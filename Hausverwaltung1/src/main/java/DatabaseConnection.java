3a2d5a6dc1c0bc22ed443d63f69271e7de994f26
AlbertoARZ <berti02berti02@gmail.com>
2024-10-11 10:30:47 +0200

    Create Interface DataBaseConnection

---
+++ Hausverwaltung1/src/main/java/DatabaseConnection.java
@@ -0,0 +1,17 @@
+public interface DatabaseConnection {
+    public DatabaseConnection(Properties properties){
+
+    }
+    void    createrAllTables(){
+
+    }
+     void truncateAllTables(){
+
+    }
+      void removeAllTables(){
+
+      }
+      void closeConnection(){
+
+      }
+}
