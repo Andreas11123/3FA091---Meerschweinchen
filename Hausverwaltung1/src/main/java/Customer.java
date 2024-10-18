f875a9135696f0c4199ce67c0951d155d0652bcf
Alex <alex.freischlad@outlook.de>
2024-10-18 10:31:29 +0200

    Add Customer new Constructor and Attributes

--- Hausverwaltung1/src/main/java/Customer.java
+++ Hausverwaltung1/src/main/java/Customer.java
@@ -2,53 +2,84 @@ import java.time.LocalDate;
 import java.util.UUID;
 
 public class Customer implements ICustomer {
+
+    // Attributes
+    private String firstName;
+    private String lastName;
+    private LocalDate birthDate;
+    private Gender gender;
+    private UUID id;
+
+    // Constructor
+    public Customer(String firstName, String lastName, LocalDate birthDate, Gender gender) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthDate = birthDate;
+        this.gender = gender;
+        this.id = UUID.randomUUID(); // Generate a random unique ID
+    }
+
+    // Default constructor
+    public Customer() {
+        this.id = UUID.randomUUID(); // Generate a random unique ID
+    }
+
+    // Getters
     @Override
     public LocalDate getBirthDate() {
-        return null;
+        return this.birthDate;
     }
 
     @Override
     public String getFirstName() {
-        return "";
+        return this.firstName;
     }
 
     @Override
     public Gender getGender() {
-        return null;
+        return this.gender;
     }
 
     @Override
     public String getLastName() {
-        return "";
-    }
-
-    @Override
-    public void setBirthDate(LocalDate birtDate) {
-
-    }
-
-    @Override
-    public void setFirstName(String firstName) {
-
-    }
-
-    @Override
-    public void setGender(Gender gender) {
-
-    }
-
-    @Override
-    public void setLastName(String lastName) {
-
+        return this.lastName;
     }
 
     @Override
     public UUID getId() {
-        return null;
+        return this.id;
+    }
+
+    // Setters
+    @Override
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    @Override
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    @Override
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 
     @Override
     public void setId(UUID id) {
+        this.id = id;
+    }
 
+    // toString method for better object representation
+    @Override
+    public String toString() {
+        return "Customer [ID=" + id + ", First Name=" + firstName + ", Last Name=" + lastName
+                + ", Birth Date=" + birthDate + ", Gender=" + gender + "]";
     }
 }
