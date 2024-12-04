5ae61d3660a23644a2430aa56223849c862d8462
Alex <alex.freischlad@outlook.de>
2024-12-04 12:07:39 +0100

    Add the beginning of the Server class

---
+++ Hausverwaltung1/src/main/java/DataConnection/Server.java
@@ -0,0 +1,14 @@
+package DataConnection;
+
+public class Server {
+
+    // Startet den Server mit der angegebenen URL
+    public static void startServer(String url) {
+        // Implementierung hier
+    }
+
+    // Stoppt den Server
+    public static void stopServer() {
+        // Implementierung hier
+    }
+}
--- Hausverwaltung1/src/main/java/Main.java
+++ Hausverwaltung1/src/main/java/Main.java
@@ -1,12 +1,11 @@
 import DataConnection.DataConnection;
 
-
 public class Main {
 
     public static void main(String[] args) {
+        // Aktueller Code: Benutzername und Verbindung testen
         System.out.println(System.getProperty("user.name"));
         DataConnection con = new DataConnection();
         con.getConnection();
-
     }
 }
